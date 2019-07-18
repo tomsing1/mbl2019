6299100db4a3fbb60924ec1d91c3d66bad034fcd
Thomas Sandmann <tomsing1@gmail.com>
2019-07-17 21:27:08 -0400

    fixed typo

--- rmbl2019/R/mbl_import_quant.R
+++ rmbl2019/R/mbl_import_quant.R
@@ -19,7 +19,7 @@ mbl_import_quantitation_results <- function(
   names(files) <- str_match(files, pattern = ".*/(.*)_S\\d*_.*/quant.sf")[, 2]
 
   gene_anno <- mbl_get_transcript_annotation(organism)
-  colnames(gene_anno) <- c("gene_id", "transcript.id", "gene_type", "symbol")
+  colnames(gene_anno) <- c("gene_id", "transcript_id", "gene_type", "symbol")
   tx2gene <- gene_anno[, c("transcript_id", "gene_id")]
 
   txi <- tximport(
